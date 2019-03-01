066317277e833161242c6ddec9d10ef083444419
ngozi-ekekwe <ngozi.ekekwe@andela.com>
2019-03-01 19:53:53 +0100

    create fleet data service

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import Car from './classes/car.js';
-import Drone  from './classes/drone.js';
+import Drone   from './classes/drone.js';
+
+import { FleetDataService } from './services/fleet-data-service.js';
+import { fleet } from './fleet-data.js'
 
 let c = new Car();
 let d = new Drone();
 
-console.log(c, d)
+let dataService = new FleetDataService();
+dataService.loadData(fleet)
+
+console.log(dataService)
+console.log(dataService.cars)
+console.log(dataService.drones)
---
+++ src/fleet-data.js
@@ -0,0 +1,44 @@
+export let fleet = [
+  {
+    license: 'ABC123',
+    type: 'drone',
+    model: 'Amazon 1250',
+    airTimeHours: '6050',
+    base: 'NEW YORK',
+    latLong: '40.77756 - 73.58900'
+  },
+  {
+  license: 'AKC123',
+  type: 'car',
+  make: 'Telsa',
+  model: 'Amazon 1250',
+  miles: '6050',
+  latLong: '40.77756 - 73.58900'
+},
+
+  {
+    license: 'EDC123',
+    type: 'drone',
+    model: 'Amazon 1250',
+    airTimeHours: '6050',
+    base: 'GREECE',
+    latLong: '40.77756 - 73.58900'
+  },
+
+  {
+    license: 'GHC123',
+    type: 'drone',
+    model: 'Amazon 1250',
+    airTimeHours: '6050',
+    base: 'YOLA',
+    latLong: '40.77756 - 73.58900'
+  },
+  {
+    license: 'AKC123',
+    type: 'car',
+    make: 'Telsa',
+    model: 'Amazon 1250',
+    miles: '6050',
+    latLong: '40.77756 - 73.58900'
+  }
+]
